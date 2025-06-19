cbbcf77e29b37cf9d0c2c943a5bc9ece5391ab6c
Kamrano22 <kamranmatnazarov7@gmail.com>
2025-06-19 20:49:03 +0500

    Update and rename Kamran to Kamran.sql

---
+++ Insert/Kamran.sql
@@ -0,0 +1,91 @@
+create database BankingSystem
+
+use BankingSystem
+
+
+create schema digital_banking 
+
+create table digital_banking.credit_cards (
+
+    CardID INT PRIMARY KEY identity,
+    CustomerID INT,
+    CardNumber VARCHAR(20) UNIQUE,
+    CardType varchar(30),
+    CVV varchar(4),               -- CVV(Card Verification Value  3-4 digit number)
+    ExpiryDate DATE,
+    CreditLimit DECIMAL(12,2),
+    Status varchar(20),                        --CustomerID FK
+	);
+
+
+	create table digital_banking.CreditCardTransactions (
+	TransactionID int primary key identity(1,1),
+	Merchant varchar (50),
+	Amount decimal(12,2),
+	Currency char(5),                               --CardID FK
+	Date date,
+	Status varchar(50)
+	);
+
+
+	create table digital_banking.OnlineBankingUsers (
+	UserID int primary key identity(1,1),
+	Username varchar(50),
+	PasswordHash numeric(20),                                  -- CustomerID fk
+	LastLogin datetime
+	);
+
+
+	create table digital_banking.BillPayments (
+	PaymentID int primary key identity(1,1),
+	BillerName varchar(30),
+	Amount numeric(20,2),
+	Date date,
+	Status varchar(20)                            --CustomerID fk
+	);
+
+
+	create table digital_banking.MobileBankingTransactions ( 
+	TransactionId int primary key identity(1,1),
+	DeviceID int,
+	AppVersion varchar(20),
+	TransactionType varchar(20),                                  --CustomerID
+	Amount numeric(12,2),
+	Date date
+	);
+
+
+	create schema loans_credits
+
+
+	create table loans_credits.Loans (
+	LoanID int primary key identity(1,1),
+	LoanType varchar(50),
+	Amount numeric (15,2),
+	InterestRate decimal (5,2),
+	StartDate date,
+	EndDate date,                                     --customerID FK
+	Status varchar(50)
+	);
+
+
+	create table loans_credits.LoanPayments (
+	PaymentID int primary key identity(1,1),
+	AmountPaid decimal(12,2),
+	PaymentDate date,                                    --LoanID Fk
+	RemainingBalance decimal(15,2)
+	);
+
+
+	create table loans_credits.CreditScores (
+	CreditScore int,                                  --customerID fk
+	UpdateAt datetime
+	);
+
+
+	create table loans_credits.DebtCollection (
+	DebtID int primary key identity,
+	AmountDue decimal(12,2),
+	DueDate date,                                          -- customerId fk
+	CollectorAssigned varchar(30)
+	);
