930fe199294a80fd7055ce401b7a7b842e1dd6c1
Reinhardt <shsulaymonov2007@gmail.com>
2025-06-27 15:35:31 +0500

    Update Kamran.sql

--- KPI/Kamran.sql
+++ KPI/Kamran.sql
@@ -9,7 +9,28 @@ FROM dbo.Accounts a
 JOIN dbo.Customers c ON c.CustomerID = a.CustomerID
 JOIN loans_credits.Loans l ON c.CustomerID = l.CustomerID
 GROUP BY a.CustomerID;
-  
+------------------------------------------------------------------------------------
+SELECT 
+    a.AccountID,
+    ROUND(
+        (
+            ISNULL(SUM(t.Amount), 0) 
+            - 
+            ISNULL(SUM(l.Amount), 0)
+        ) * 100.0 / NULLIF(a.Balance, 0), 
+    2) AS return_on_assets
+FROM 
+    Accounts a
+LEFT JOIN dbo.Transactions t ON a.AccountID = t.AccountID AND t.Status = 'Completed'
+LEFT JOIN dbo.Customers c ON a.CustomerID = c.CustomerID
+LEFT JOIN loans_credits.Loans l ON c.CustomerID = l.CustomerID
+GROUP BY 
+    a.AccountID, a.Balance;
+
+	--- here i calculated ROA(Return on assets) profitability indicator  which tells us how banks are using  assets (loans reserves,investments) to generate profit
+
+
+
 
 
 
